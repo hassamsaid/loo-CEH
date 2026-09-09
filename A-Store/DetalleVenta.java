4d8bdd862db48b25f181e527787427e43f87e7bd
ELCARLANGAS45 <carlos.bedolla@anahuac.mx>
2026-09-09 14:09:27 -0500

    mi parte de detalle de venta

---
+++ A-Store/DetalleVenta.java
@@ -0,0 +1,56 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class DetalleVenta {
+    
+    private int id;
+    private Producto producto; 
+    private Map<String, String> variante; 
+    private int cantidad;
+    private float precio_unitario;
+    private float subtotal_linea;
+
+    public DetalleVenta(int id, Producto producto, int cantidad, float precio_unitario) {
+        this.id = id;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precio_unitario = precio_unitario;
+        this.variante = new HashMap<>(); 
+        
+        this.calcular_subtotal_linea(); 
+    }
+
+    public int get_id() {
+        return this.id;
+    }
+
+    public Producto get_producto() {
+        return this.producto;
+    }
+
+    public Map<String, String> get_variante() {
+        return this.variante;
+    }
+
+    public int get_cantidad() {
+        return this.cantidad;
+    }
+
+    public void set_cantidad(int cantidad) {
+        this.cantidad = cantidad;
+        this.calcular_subtotal_linea(); 
+    }
+
+    public float get_precio_unitario() {
+        return this.precio_unitario;
+    }
+
+    public float calcular_subtotal_linea() {
+        this.subtotal_linea = this.cantidad * this.precio_unitario;
+        return this.subtotal_linea;
+    }
+
+    public float get_subtotal_linea() {
+        return this.subtotal_linea;
+    }
+}
