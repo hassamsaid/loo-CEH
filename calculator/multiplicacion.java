e509e39465f29f0fd2d65ade94eb6295ee2ff4fa
emmanuelopi <emmanuel@opiliosoft.com>
2026-09-14 12:33:51 -0500

    borrado de archivos innecesarios

--- calculator/multiplicacion.java
+++
@@ -1,9 +0,0 @@
-public class Multiplicacion {
-
-    public static double Hacer_Multiplicacion(double numero1, double numero2) {
-        return numero1 * numero2;
-    }
-    public static double Hacer_Division(double numero1, double numero2) {
-        return numero1 / numero2;
-    }
-}
